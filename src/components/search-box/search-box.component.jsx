83e5cb9d12ae680ef332eb5f35014b272e54c444
Herman Adu <herman.adu@hotmail.com>
2020-03-12 17:57:34 +0000

    first commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, hanldeChange }) => (
+    <input
+          className='search'
+          type ='search' 
+          placeholder = { placeholder }
+          onChange = { hanldeChange }
+        /> 
+);
