b9e8bd5082d8e14398a974c1a899b5eea8d4c638
Herman Adu <herman.adu@hotmail.com>
2020-03-12 18:04:05 +0000

    Updates

---
+++ precache-manifest.b82af79779ccb7cb63981e38610c6e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b181375a2cd8aee57f598d4703714b2b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "700fddfa824cca910e8e",
+    "url": "/monsters-rolodex/static/css/main.01952b3c.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700fddfa824cca910e8e",
+    "url": "/monsters-rolodex/static/js/main.579104d7.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
